515bcb22396d0fa594f160d1b8114e774522c43a
Marcio Vinicius de Melo de Alvarenga <164529046+MarcioAlvarenga-AlfaDaemon@users.noreply.github.com>
2026-07-02 09:44:00 -0300

    s

--- main.js
+++ main.js
@@ -1,22 +1,27 @@
-const numeroSenha=document.querySelector('.parametro-senha_texto');
+const numeroSenha = document.querySelector('.parametro-senha_texto');
+
 let tamanhoSenha = 12;
-numeroSenha.textContent=tamanhoSenha;
-constbotoes=document.querySelectorAll('.parametro-senha_botao');
-botoes[0].onclick=diminuiTamanho;
-botoes[1].onclick=diminuiTamanho;
-function diminuiTamanho(){
-    ife (tamanhoSenha > 1) {
-        // tamanhoSenha = tamanhoSenha-1;
-        tamanhoSenha--;
-}
 numeroSenha.textContent = tamanhoSenha;
-geraSenha();
+
+const botoes = document.querySelectorAll('.parametro-senha_botao');
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+function diminuiTamanho() {
+    if (tamanhoSenha > 1) {
+        tamanhoSenha--;
+    }
+
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
 }
-function aumentaTamanho(){
-    if(tamanhoSenha < 20){
-        //tamanhoSenha = tamanhoSenha+1;
+
+function aumentaTamanho() {
+    if (tamanhoSenha < 20) {
         tamanhoSenha++;
     }
+
     numeroSenha.textContent = tamanhoSenha;
     geraSenha();
 }
